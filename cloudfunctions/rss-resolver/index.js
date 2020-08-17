a570d4f6a8a767f9300964b2da673e88648c7028
KylinLee <kylincottage@163.com>
2020-08-17 23:49:24 +0800

    xml解析
    完成xml解析，链接提取，测试数据库，测试静态文件上传。

--- cloudfunctions/rss-resolver/index.js
+++ cloudfunctions/rss-resolver/index.js
@@ -1,8 +1,9 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-const convert = require('xml-js');
-const http = require("http");
-const https = require("https")
+// const convert = require('xml-js');
+const axios = require("axios")
+// var parser = require('fast-xml-parser');
+var parseString = require('xml2js').parseString;
 
 cloud.init({
     env: cloud.DYNAMIC_CURRENT_ENV
@@ -11,23 +12,19 @@ cloud.init({
 // 云函数入口函数
 exports.main = async (props, context) => {
     const wxContext = cloud.getWXContext()
-    const links = ["https://www.ithome.com/rss/"]
-    // try{
-    //     links.forEach(link => {
-    //     const res = http.get(link)
-    //     console.log(res)
-    // });
-    // }catch{
-    //     links.forEach(link => {
-    //         const res = http.get(link)
-    //         console.log(res)})
-    // }
-    const res = https.get("https://www.ithome.com/rss/")
-    console.log(res)
+    const link = "https://www.ithome.com/rss/"
+    let xml;
+    axios.get(link).then((res)=>{
+        xml = res.data
+    }).catch(err=>{
+        console.warn(err)
+    })
     
-    var result1 = convert.xml2json(xml, { compact: true, spaces: 4 });
-    var result2 = convert.xml2json(xml, { compact: false, spaces: 4 });
-    console.log(result1, "\n", result2)
+    var result1 = parseString(xml,(err, result) => {
+        console.dir(result);
+    });
+    // var result2 = convert.xml2json(xml, { compact: false, spaces: 4 });
+    console.log(result1)
 
     return {
         props,
