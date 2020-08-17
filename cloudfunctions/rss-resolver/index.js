65cb046c552490d840b9106ee75ed91031933dfb
KylinLee <kylincottage@163.com>
2020-08-17 23:49:24 +0800

    day end
    完成xml解析，链接提取，测试数据库，测试静态文件上传。

--- cloudfunctions/rss-resolver/index.js
+++ cloudfunctions/rss-resolver/index.js
@@ -1,30 +1,65 @@
-// 云函数入口文件
 const cloud = require('wx-server-sdk')
-// const convert = require('xml-js');
 const axios = require("axios")
-// var parser = require('fast-xml-parser');
-var parseString = require('xml2js').parseString;
+const parser = require('fast-xml-parser');
 
 cloud.init({
-    env: cloud.DYNAMIC_CURRENT_ENV
+    env: "inner-test-m2c3m"
 })
+const db = cloud.database()
 
-// 云函数入口函数
+function parseImage(el, buffer) {
+    for (const key in el) {
+        if (el.hasOwnProperty("img")) {
+            buffer.push(el["img"]["@_src"])
+        } else if (typeof el[key] === "object") {
+            parseImage(el[key], buffer)
+        }
+    }
+    console.log(buffer)
+}
+
 exports.main = async (props, context) => {
     const wxContext = cloud.getWXContext()
+    // const RSS_SOURCE = db.collection('RSS_SOURCE').get().then(res => {
+    //     // res.data 包含该记录的数据
+    //     console.log(res.data)
+    // })
     const link = "https://www.ithome.com/rss/"
-    let xml;
-    axios.get(link).then((res)=>{
-        xml = res.data
-    }).catch(err=>{
-        console.warn(err)
+    const xml = await axios.get(link).then((res) => {
+        return res.data
+    }).catch(err => {
+        console.err(err)
     })
-    
-    var result1 = parseString(xml,(err, result) => {
-        console.dir(result);
-    });
-    // var result2 = convert.xml2json(xml, { compact: false, spaces: 4 });
-    console.log(result1)
+    const source = parser.parse(xml)
+    console.log(source)
+    const imageArr = []
+    const postChannel = source["rss"]["channel"]["title"]
+    for (item of source["rss"]["channel"]["item"]) {
+        let postTitle = item["title"]
+        let pubDate = item["pubDate"]
+        let linkKey = item["link"]
+        let description = item["description"]
+        let content = item["content"] || item["content:encoded"] || description
+        content = parser.parse(content, { ignoreAttributes: false })
+        console.log(typeof content)
+        parseImage(content, imageArr)
+    }
+
+    console.log(imageArr)
+
+    // const img = await axios.get("https://img.ithome.com/newsuploadfiles/2020/8/20200817_112229_307.jpeg", {
+    //     responseType: 'arraybuffer'
+    // }).then((res) => {
+    //     return res.data
+    // })
+
+    // await cloud.uploadFile({
+    //     cloudPath: "20200817_112229_307.jpeg",
+    //     fileContent: img
+    // }).then((res) => {
+    //     console.warn(res)
+    // })
+
 
     return {
         props,
