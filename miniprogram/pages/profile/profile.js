f6ee967be3a87cc04518244b2f96b9b1e7fe9962
KylinLee <lee.kylin@outlook.com>
2020-09-17 12:16:04 +0800

    tab
    完善tab切换

--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -1,54 +1,29 @@
 // miniprogram/pages/profile.js
-Page({
-    alert() {
-
+Component({
+    options: {
+        addGlobalClass: true,
     },
-    getUserInfo(userInfo) {
-        const that = this
-        const { nickName, avatarUrl } = userInfo.detail.userInfo
-        wx.cloud.callFunction({
-            name: "login",
-            data: {
-                nickName,
-                avatarUrl
-            }
-        }).then((res) => {
-            console.log(res)
-            that.setData({
-                nickName: res["result"]["nickName"],
-                avatarUrl: res["result"]["avatarUrl"]
+    methods:{
+        getUserInfo(userInfo) {
+            const that = this
+            const { nickName, avatarUrl } = userInfo.detail.userInfo
+            wx.cloud.callFunction({
+                name: "login",
+                data: {
+                    nickName,
+                    avatarUrl
+                }
+            }).then((res) => {
+                console.log(res)
+                that.setData({
+                    nickName: res["result"]["nickName"],
+                    avatarUrl: res["result"]["avatarUrl"]
+                })
             })
-        })
+        }
     },
     data: {
         nickName: "欢迎",
         avatarUrl: ""
-    },
-    onLoad: function (options) {
-        wx.checkSession({
-          success: (res) => {console.log(res)},
-          fail: (res)=>{console.log(res)}
-        })
-    },
-    onReady: function () {
-
-    },
-    onShow: function () {
-
-    },
-    onHide: function () {
-
-    },
-    onUnload: function () {
-
-    },
-    onPullDownRefresh: function () {
-
-    },
-    onReachBottom: function () {
-
-    },
-    onShareAppMessage: function () {
-
     }
 })
